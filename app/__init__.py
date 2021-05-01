8ff32aceabeab8a5a3deccfdfa17c112c8cf8e19
Saejin Kwak Tanguay <saejinkwak@gmail.com>
2021-05-01 12:10:56 -0700

    set up routes, added books model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
+
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development' 
+
+    db.init_app(app) 
+    migrate.init_app(app, db) 
+
+    from .routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
+    from app.models.book import Book 
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route("/hello-world", methods =["GET"])
+
+def say_hello_world():
+    response_body = "Hello, World!"
+    return response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello!", 
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+
+
